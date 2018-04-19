c1aa768ff81e78a94e94c4eb5a010122109b8691
HaVuNguyen <32772746+HaVuNguyen@users.noreply.github.com>
2018-04-19 08:54:58 +0700

    update

--- DemoUnboundService_PlaySong/app/src/main/java/com/example/dell/demounboundservice_playsong/MainActivity.java
+++ DemoUnboundService_PlaySong/app/src/main/java/com/example/dell/demounboundservice_playsong/MainActivity.java
@@ -6,16 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-import java.util.Map;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    int position = 0;
+    ArrayList<PlaySongService> arraySong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AddSong();
+    }
+
+    private void AddSong() {
+
     }
 
     @SuppressLint("ResourceType")
@@ -36,11 +42,16 @@ public class MainActivity extends AppCompatActivity {
         this.stopService(intent);
     }
 
-//    @SuppressLint("ResourceType")
-//    public void nextSong(View view) {
-//        ImageButton imageButton ;
-//        imageButton = (ImageButton) findViewById(R.drawable.ic_next);
-//        Intent intent = new Intent(MainActivity.this, PlaySongService.class);
-//
-//    }
+
+    @SuppressLint("ResourceType")
+    public void nextSong(View view) {
+        ImageButton btnNext;
+        btnNext = (ImageButton) findViewById(R.drawable.ic_next);
+        Intent intent = new Intent(MainActivity.this,PlaySongService.class);
+        //this.nextSerice(intent);
+
+    }
+
+    public void nextsong(View view) {
+    }
 }
--- DemoUnboundService_PlaySong/app/src/main/java/com/example/dell/demounboundservice_playsong/PlaySongService.java
+++ DemoUnboundService_PlaySong/app/src/main/java/com/example/dell/demounboundservice_playsong/PlaySongService.java
@@ -4,15 +4,12 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PlaySongService extends Service {
     private MediaPlayer mediaPlayer1;
-    private MediaPlayer mediaPlayer2;
-    private MediaPlayer mediaPlayer3;
-    private MediaPlayer mediaPlayer4;
-    public PlaySongService() {
-    }
-
+    ImageButton btnNext,btnPrev;
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -22,20 +19,24 @@ public class PlaySongService extends Service {
     public void onCreate(){
         super.onCreate();
         mediaPlayer1 = MediaPlayer.create(getApplicationContext(), R.raw.mysong);
-//        mediaPlayer2 = MediaPlayer.create(getApplicationContext(), R.raw.mysong2);
-//        mediaPlayer3 = MediaPlayer.create(getApplicationContext(), R.raw.mysong3);
-//        mediaPlayer4 = MediaPlayer.create(getApplicationContext(), R.raw.mysong4);
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId){
-        mediaPlayer1.start();
+        if(mediaPlayer1.isPlaying()){
+            mediaPlayer1.pause();
+        }else{
+            mediaPlayer1.start();
+        }
         return START_STICKY;
     }
 
+
     @Override
     public void onDestroy(){
+        mediaPlayer1.stop();
         mediaPlayer1.release();
         super.onDestroy();
 
     }
+
 }
